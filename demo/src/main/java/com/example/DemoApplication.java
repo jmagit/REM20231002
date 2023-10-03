a56987f66df871b344bcbbec55af81ceb0669f6d
Javier <jmartin@grupoloyal.com>
2023-10-03 11:24:52 +0200

    CRUD

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.domains.contracts.repositories.ActorRepository;
+import com.example.domains.entities.Actor;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -21,6 +22,16 @@ public class DemoApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		System.out.println("Aplicacion Arrancada ....");
 		
+//		var actor = new Actor(0, "Pepito", "Grillo");
+//		dao.save(actor);
+//		var item = dao.findById(201);
+//		if(item.isPresent()) {
+//			var actor = item.get();
+//			actor.setFirstName(actor.getFirstName().toUpperCase());
+//			dao.save(actor);
+//			
+//		}
+		dao.deleteById(201);
 		dao.findAll().forEach(System.out::println);
 	}
 
