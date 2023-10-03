45138a6f5e60ae903d9893835dc81a749e536322
Javier <jmartin@grupoloyal.com>
2023-10-03 12:31:01 +0200

    consultas

--- demo/src/main/java/com/example/domains/contracts/repositories/ActorRepository.java
+++ demo/src/main/java/com/example/domains/contracts/repositories/ActorRepository.java
@@ -1,9 +1,22 @@
 package com.example.domains.contracts.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.domains.entities.Actor;
 
-public interface ActorRepository extends JpaRepository<Actor, Integer> {
 
+public interface ActorRepository extends JpaRepository<Actor, Integer>, JpaSpecificationExecutor<Actor> {
+	List<Actor> findTop5ByFirstNameStartingWithOrderByLastNameDesc(String prefijo);
+	List<Actor> findTop5ByFirstNameStartingWith(String prefijo, Sort orderBy);
+	List<Actor> findByActorIdGreaterThan(int id);
+	@Query(value = "from Actor a where a.actorId > ?1")
+	List<Actor> findNovedadesJPQL(int id);
+	@Query(value = "select * from actor where actor_id > :id", nativeQuery = true)
+	List<Actor> findNovedadesSQL(@Param("id") int id);
 }
