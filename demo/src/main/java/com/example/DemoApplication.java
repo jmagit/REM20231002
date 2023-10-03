304591eb2d8ca85475c9350b2fc0e53ab4c68b8f
Javier <jmartin@grupoloyal.com>
2023-10-03 11:14:31 +0200

    repositorio

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,8 +1,11 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.domains.contracts.repositories.ActorRepository;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -11,9 +14,14 @@ public class DemoApplication implements CommandLineRunner {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Autowired
+	ActorRepository dao;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Aplicacion Arrancada ....");
+		
+		dao.findAll().forEach(System.out::println);
 	}
 
 }
